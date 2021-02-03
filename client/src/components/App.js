41b6687aa7256542625d37390ce7c365ffb60987
TedtheDev <tylerjedmunds@gmail.com>
2021-02-02 20:54:22 -0700

    setup react app with login

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Login from './Login';
 
-function App() {
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+const App = () => {
+  // TODO: make login path actually login
+  // and home path the root
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Switch>
+        <Route path='/'>
+          <Login />
+        </Route>
+        <Route path='/home'>
+          <div>Home</div>
+        </Route>
+      </Switch>
+    </Router>
+  )
 }
 
 export default App;
